95d4626f0ec9c8ac51e569d3bf51c1295e17ad6a
JoseJCanedo <canedo_jose@columbusstate.edu>
2025-04-22 04:31:16 +0000

    api

---
+++ server/externalApi/externalApiCalls.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const apiKey = 'd606319884e941d79e731601252204'
+
+module.exports = {
+    forecastApi: async (location, days) => {
+        return await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=${days}&aqi=no&alerts=no`)
+            .then(function (response) {
+                return response;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return{ error: "Unable to fetch forecast information." }
+            })
+        // return results;
+    },
+    // add alert API call hint: change forecast.json to match api wanted 
+    // add current API
+    // documentation https://www.weatherapi.com/docs/#intro-request
+}
--- server/index.js
+++ server/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const circularJSON = require('circular-json');
+const { forecastApi } = require('./externalApi/externalApiCalls'); // add additional functions comma separated example { foo, bar }
 
 const app = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define a simple API route
-app.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
+app.get('/forecast', async (req, res) => {  // example /forecast?location=31909&days=3
+    try {
+        const result = await forecastApi(req.query.location, req.query.days);
+        res.send(circularJSON.stringify(result));
+    } catch (error) {
+        console.error("Error in forecast function:", error);
+        res.status(500).json({ error: "An error occurred" });
+    }
 });
 
 // Start the server
